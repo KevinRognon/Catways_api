868a42a2c7f4c655e2c70984d306f529e1285751
Kevin Rognon <rognonk@gmail.com>
2024-08-04 16:26:25 +0200

    Ajout de la suppression des réservation liées au catway supprimé

--- routes/catways.js
+++ routes/catways.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const service_catway = require('../services/catway');
-
 const private_route = require('../middlewares/private');
-const {body, validationResult} = require("express-validator");
-const moment = require('moment');
 
 // Récupérer tous les catways
 router.get('/', private_route.checkJWT, service_catway.getAllCatways);
@@ -62,115 +59,6 @@ router.delete('/:id', private_route.checkJWT, async function(req, res) {
 		return res.status(501);
 	}
 });
-
-// =========== ROUTES RESERVATIONS ============
-
-// router.get('/:id/reservations', private_route.checkJWT, async function(req, res) {
-// 	try {
-// 		const catway = await service_catway.getCatwayById(req.params.id);
-// 		const reservations = await service_reservations.getReservationsOfCatway(req.params.id);
-// 		return res.render('catways/reservations/reservations', {
-// 			reservations: reservations,
-// 			catway: catway,
-// 			moment: moment,
-// 			user: req.session.user
-// 		})
-
-// 	} catch (e) {
-// 		return res.status(501);
-// 	}
-// });
-
-// router.post('/:id/reservations', private_route.checkJWT, async function (req, res) {
-// 	const id = req.params.id;
-// 	const catway = await service_catway.getCatwayById(id);
-// 	const temp = {
-// 		catwayNumber : catway.catwayNumber,
-// 		clientName   : req.body.client_name,
-// 		boatName     : req.body.boat_name,
-// 		checkIn      : req.body.checkIn,
-// 		checkOut     : req.body.checkOut
-// 	};
-
-// 	try {
-// 		let resa = await service_reservations.createReservation(temp);
-// 		let reservations = await service_reservations.getReservationsOfCatway(id);
-
-
-// 		if(resa) {
-// 			return res.render('catways/reservations/reservations', {
-// 				reservations: reservations,
-// 				catway: catway,
-// 				moment: moment,
-// 				user: req.session.user,
-// 				message: "Réservation enregistrée"
-// 			});
-// 		}
-// 		return res.render('catways/reservations/reservations', {
-// 			reservations: reservations,
-// 			catway: catway,
-// 			moment: moment,
-// 			user: req.session.user,
-// 			error_message: "Echec de la reservation"
-// 		})
-// 	} catch (e) {
-// 		return res.status(501);
-// 	}
-// });
-
-// router.get('/:id_catway/reservations/:id_reservation', private_route.checkJWT, async function (req, res) {
-// 	console.log("Detail d'une reservation");
-// 	const id_reservation = req.params.id_reservation;
-// 	const id_catway = req.params.id_catway;
-// 	const reservation = await service_reservations.getReservationById(id_reservation);
-// 	const reservations = await service_reservations.getReservationsOfCatway(id_catway);
-// 	const catway = await service_catway.getCatwayById(id_catway);
-
-// 	if(!reservation) {
-// 		return res.render('catways/reservations/reservations', {
-// 			reservations: reservations,
-// 			catway: catway,
-// 			moment: moment,
-// 			user: req.session.user,
-// 			error_message: "Echec de l'ouverture de la page"
-// 		})
-// 	}
-
-// 	return res.render('catways/reservations/detail_reservation', {
-// 		catway: catway,
-// 		reservation: reservation,
-// 		moment: moment,
-// 		user: req.session.user,
-// 	})
-
-
-// });
-
-
-// router.get('/:id_catway/reservations/:id_reservation/delete', private_route.checkJWT, async function (req, res) {
-// 	const catway = await service_catway.getCatwayById(req.params.id_catway);
-
-// 	try {
-// 		let deleted = await service_reservations.deleteReservation(req.params.id_reservation);
-// 		const reservations = await service_reservations.getReservationsOfCatway(req.params.id_catway);
-// 		console.log(deleted);
-
-// 		return res.render('catways/reservations/reservations', {
-// 			reservations: reservations,
-// 			catway: catway,
-// 			moment: moment,
-// 			user: req.session.user,
-// 			message: "Suppression effectuée"
-// 		})
-// 	} catch (e) {
-// 		return res.status(501);
-// 	}
-
-// });
-
-
-
-
 
 
 
